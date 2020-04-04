2827020c1cb7986a494c5e9e8c90532f8df4ca97
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2020-04-04 14:11:10 +0530

    Welcome page done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import './App.css';
-import Login from './Pages/Login';
-import Welcome from './Pages/Welcome';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import NoSsr from "@material-ui/core/NoSsr";
+import { createMuiTheme } from "@material-ui/core/styles";
+import Login from "./Pages/Login";
+import Welcome from "./Pages/Welcome";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: "http://localhost:4000",
 });
+
+const theme = createMuiTheme();
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path={"/"}>
-              <Welcome />
-            </Route>
-            <Route path={"/login"}>
-              <Login />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <NoSsr>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <div className="App">
+              <Switch>
+                <Route path={"/"} exact>
+                  <Welcome />
+                </Route>
+                <Route path={"/login"}>
+                  <Login />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </ThemeProvider>
+      </NoSsr>
     </ApolloProvider>
   );
 }
--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Button from '@material-ui/core/Button';
+import { useHistory } from "react-router-dom";
+
+const StyledWelcome = styled.div`
+  ${({ theme }) => `
+		background-color: ${theme.palette.primary.main};
+		color: #fff;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	`}
+`;
+
+const Row = styled.div`
+	& > * {
+		margin: 8px !important;
+	}
+	display: flex;
+`
+
+const StyledH1 = styled.h1`
+	${({theme}) => `
+    padding: ${theme.spacing(1)}px;
+	`}
+`
 
 const Welcome = () => {
-	return (
-		<div>
-			Welcome
-		</div>
-	)
-}
+	let history = useHistory();
 
-export default Welcome
+	const handleClick = (route) =>  {
+		history.push(route)
+	}
+  return (
+    <StyledWelcome>
+      <StyledH1>Welcome to Humane</StyledH1>
+			<Row>
+				<Button variant="contained" color="secondary" onClick={() => handleClick("/login")}>Login</Button>
+				<Button variant="contained" color="secondary">Sign up</Button>
+			</Row>
+    </StyledWelcome>
+  );
+};
+
+export default Welcome;
